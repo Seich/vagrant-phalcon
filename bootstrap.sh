4a0cf66c33b5d1640bc4e97212e83047003bec15
Sergio Diaz <seich@martianwabbit.com>
2013-08-07 23:10:01 -0600

    Changed from postgresql to mysql.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update
 
-sudo apt-get install -y gcc git autoconf build-essential
+sudo apt-get install -y gcc git autoconf build-essential curl
 
 echo 'Installing Apache.'
 sudo apt-get install -y apache2
@@ -12,10 +12,10 @@ echo 'Installing PHP5.'
 sudo apt-get install -y php5 libapache2-mod-php5
 sudo a2enmod php5
 
-echo 'Installing Postgresql.'
-sudo apt-get install -y \
-postgresql postgresql-contrib postgresql-client phppgadmin \
-php5-pgsql php5-dev
+echo 'Installing MySql.'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql php5-dev
 
 echo 'Installing Phalcon.'
 git clone git://github.com/phalcon/cphalcon.git
@@ -23,5 +23,10 @@ cd cphalcon/build
 sudo ./install
 
 echo 'extension=phalcon.so' | sudo tee -a /etc/php5/apache2/php.ini
+echo 'extension=phalcon.so' | sudo tee -a /etc/php5/cli/php.ini
 
 service apache2 restart
+
+echo 'Installing Composer'
+curl -s http://getcomposer.org/installer | php
+sudo mv composer.phar /usr/bin/composer
